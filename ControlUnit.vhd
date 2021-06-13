ceb41e05d55c7f06d9e2641a2419a82bd8a7cf07
aya1sameh <49209317+aya1sameh@users.noreply.github.com>
2021-06-13 22:12:29 +0200

    Adding MemStage and updating ControlUnit

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -4,12 +4,12 @@ USE IEEE.std_logic_1164.ALL;
 Entity ControlUnit is port(
 OpCode:in std_logic_vector(5 downto 0);
 Reset:in std_logic;
-ControlSignals:out std_logic_vector(19 downto 0)
+ControlSignals:out std_logic_vector(18 downto 0)
 );
 end ControlUnit;
 
 Architecture ContolUnit_Arch of ControlUnit is
-Signal CSTemp:std_logic_vector(19 downto 0);
+Signal CSTemp:std_logic_vector(18 downto 0);
 
 begin 
 process(Reset,OpCode)is begin
@@ -20,63 +20,63 @@ process(Reset,OpCode)is begin
 		if(OpCode="000000")then --NOP
 			CSTemp<=(others=>'0');
 		elsif(OpCode="000001")then --SETC
-			CSTemp<="00001000000000000010";
+			CSTemp<="0000100000000000010";
 		elsif(OpCode="000010")then --CLRC
-			CSTemp<="00010000000000000010";
+			CSTemp<="0001000000000000010";
 		elsif(OpCode="000011")then --NOT Rdst
-			CSTemp<="00011100000010000010";
+			CSTemp<="0001110000001000010";
 		elsif(OpCode="000100")then --INC Rdst
-			CSTemp<="00100100000010000010";
+			CSTemp<="0010010000001000010";
 		elsif(OpCode="000101")then --DEC Rdst
-			CSTemp<="00101100000010000010";
+			CSTemp<="0010110000001000010";
 		elsif(OpCode="000110")then --OUT Rdst
-			CSTemp<="00000100000100000000";
+			CSTemp<="0000010000010000000";
 		elsif(OpCode="000111")then --IN Rdst
-			CSTemp<="00000100001010000000";
+			CSTemp<="0000010000101000000";
 
 		--------------TWO OPERANDs------------
 		elsif(OpCode="001000")then --MOV Rsrc,Rdst
-			CSTemp<="00110100000010000000";
+			CSTemp<="0011010000001000000";
 		elsif(OpCode="001001")then --ADD Rsrc,Rdst
-			CSTemp<="00111100000010000010";
+			CSTemp<="0011110000001000010";
 		elsif(OpCode="001010")then --IADD Rdst,Imm
-			CSTemp<="01000010000010000010";
+			CSTemp<="0100001000001000010";
 		elsif(OpCode="001011")then --SUB Rsrc,Rdst
-			CSTemp<="01001100000010000010";
+			CSTemp<="0100110000001000010";
 		elsif(OpCode="001100")then --AND Rsrc,Rdst
-			CSTemp<="01010100000010000010";
+			CSTemp<="0101010000001000010";
 		elsif(OpCode="001101")then --OR Rsrc,Rdst
-			CSTemp<="01011100000010000010";
+			CSTemp<="0101110000001000010";
 		elsif(OpCode="001110")then --SHL Rdst,Imm
-			CSTemp<="01100010000010000010";
+			CSTemp<="0110001000001000010";
 		elsif(OpCode="001111")then --SHR Rdst,Imm
-			CSTemp<="01101010000010000010";
+			CSTemp<="0110101000001000010";
 
 		----------------MEMORY----------------
 		elsif(OpCode="010000")then --PUSH Rdst
-			CSTemp<="00000101010001000000";
+			CSTemp<="0000010101000100000";
 		elsif(OpCode="010001")then --POP Rdst
-			CSTemp<="00000100110010011000";
+			CSTemp<="0000010011001001000";
 		elsif(OpCode="010010")then --LDM Rdst,Imm
-			CSTemp<="00110010000010000000";
+			CSTemp<="0011001000001000000";
 		elsif(OpCode="010011")then --LDD Rdst,Offset(Rsrc)
-			CSTemp<="01110100000010011000";
+			CSTemp<="0111010000001001000";
 		elsif(OpCode="010100")then --STD Rdst,Offset(Rsrc)
-			CSTemp<="01111100000001000000";
+			CSTemp<="0111110000000100000";
 
 		----------------BRANCH OP-------------
 		elsif(OpCode="010101")then --JZ Rdst
-			CSTemp<="10000100000000000110";
+			CSTemp<="1000010000000000110";
 		elsif(OpCode="010110")then --JN Rdst
-			CSTemp<="10001100000000000110";
+			CSTemp<="1000110000000000110";
 		elsif(OpCode="010111")then --JC Rdst
-			CSTemp<="10010100000000000110";
+			CSTemp<="1001010000000000110";
 		elsif(OpCode="011000")then --JMP Rdst
-			CSTemp<="10011100000000000100";
+			CSTemp<="1001110000000000100";
 		elsif(OpCode="011001")then --CALL Rdst
-			CSTemp<="00000101010001100100";
+			CSTemp<="0000010101000110100";
 		elsif(OpCode="011010")then
-			CSTemp<="00000100110000001001";
+			CSTemp<="0000010011000000001";
 		else --unknown op
 			CSTemp<=(others=>'0');
  		end if;
---
+++ MemoryStage.vhd
@@ -0,0 +1,57 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+Entity MemoryStage is 
+Generic (n:integer:= 32);
+port(
+Clock,Reset : in std_logic;
+Rdst_Data,SP,PC,AluOutput : in std_logic_vector((n-1) downto 0);
+ControlSignals: in std_logic_vector(19 downto 0);
+MemOutput,SP_Out, PC_Out: out std_logic_vector((n-1) downto 0)
+);
+end MemoryStage;
+
+Architecture MemoryStage_Arch of MemoryStage is 
+
+Component Memory is
+Generic (n:integer:= 32; SpaceSize:integer:= 1000);
+port(
+Clock,Mem_Write,Reset: in std_logic;
+Address : in std_logic_vector( (n - 1) downto 0);
+Write_Data : in std_logic_vector( (n - 1) downto 0);
+Read_Data : out std_logic_vector( (n - 1) downto 0)
+);
+end Component;
+Signal MemWriteSignal,SPSrc,PC_To_Mem: std_logic;
+Signal Address,Write_Data,Read_Data,CurrentSP : std_logic_vector( (n - 1) downto 0);
+Signal SP_OP:std_logic_vector( 1 downto 0);
+begin
+MemWriteSignal<=ControlSignals(5); 
+
+SPSrc<=ControlSignals(9);
+SP_OP( 1 downto 0)<=ControlSignals(11 downto 10);
+
+CurrentSP<=std_logic_vector(to_unsigned(to_integer(unsigned(SP)) - 2,CurrentSP'length )) when  SP_OP="10" and Clock = '1'
+else std_logic_vector(to_unsigned(to_integer(unsigned(SP)) + 2,CurrentSP'length )) when  SP_OP="01" and Clock = '1'
+else (others => '0') when Reset='1'
+else SP;
+
+Address<=CurrentSP when SPSrc='1' and MemWriteSignal='0'
+else SP when SPSrc='1' and MemWriteSignal='1'
+else AluOutput when SPSrc='0' and MemWriteSignal='1'
+else (others => '0');
+
+PC_To_Mem<=ControlSignals(4);
+Write_Data<= PC when PC_To_Mem='1'
+else Rdst_Data;
+
+
+DataMemory:Memory GENERIC MAP(n/2,1048576) PORT MAP(Clock, MemWriteSignal, Reset, Address, Write_Data, Read_Data);
+
+MemOutput<=(OTHERS => '0') WHEN Reset = '1'
+else Read_Data;
+
+SP_Out<=CurrentSP;
+
+end MemoryStage_Arch;
