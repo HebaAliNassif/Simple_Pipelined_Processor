fbfaf44385f64005508adf41deb41f660ff09491
aya1sameh <49209317+aya1sameh@users.noreply.github.com>
2021-06-15 18:25:58 +0200

    Correcting DataForwarding

--- DataForward.vhd
+++ DataForward.vhd
@@ -3,7 +3,7 @@ USE IEEE.std_logic_1164.ALL;
 
 ENTITY DataForward IS
 	PORT (
-		MemRead,RegWriteEnable, MemWriteEnable, MemToRegEnable : IN STD_LOGIC;
+		MemRead_ID_IE,RegWriteEnable_IE_MEM, RegWriteEnable_MEM_WB, MemToRegEnable_MEM_WB : IN STD_LOGIC;
 		ReadDataIndex2_ID_IE, ReadDataIndex2_IE_Mem, ReadDataIndex2_Mem_WB, ReadDataIndex1_ID_IE : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 		ReadDataIndex1_ID, ReadDataIndex2_ID : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 		ALUOutput_IE_Mem, ALUOutput_Mem_WB, MemOutput_Mem_WB : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -12,30 +12,30 @@ ENTITY DataForward IS
 END DataForward;
 ARCHITECTURE DataForward_Arch OF DataForward IS BEGIN
 
-	ReadData1_Forward_Enable <= '1' WHEN (RegWriteEnable = '1' AND ReadDataIndex2_IE_Mem = ReadDataIndex1_ID) OR (MemWriteEnable = '1' AND ReadDataIndex2_Mem_WB = ReadDataIndex1_ID)
+	ReadData1_Forward_Enable <= '1' WHEN (RegWriteEnable_IE_MEM = '1' AND ReadDataIndex2_IE_Mem = ReadDataIndex1_ID) OR (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex2_Mem_WB = ReadDataIndex1_ID)
 		ELSE
 		'0';
 
-	ReadData2_Forward_Enable <= '1' WHEN (RegWriteEnable = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_IE_Mem) OR (MemWriteEnable = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB)
+	ReadData2_Forward_Enable <= '1' WHEN (RegWriteEnable_IE_MEM = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_IE_Mem) OR (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB)
 		ELSE
 		'0';
-	ReadData1_Forward <= ALUOutput_IE_Mem WHEN (RegWriteEnable = '1' AND ReadDataIndex1_ID = ReadDataIndex2_IE_Mem)
+	ReadData1_Forward <= ALUOutput_IE_Mem WHEN (RegWriteEnable_IE_MEM = '1' AND ReadDataIndex1_ID = ReadDataIndex2_IE_Mem)
 		ELSE
-		ALUOutput_Mem_WB WHEN (MemWriteEnable = '1' AND ReadDataIndex1_ID = ReadDataIndex2_Mem_WB) AND MemToRegEnable = '0'
+		ALUOutput_Mem_WB WHEN (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex1_ID = ReadDataIndex2_Mem_WB) AND MemToRegEnable_MEM_WB = '0'
 		ELSE
-		MemOutput_Mem_WB WHEN (MemWriteEnable = '1' AND ReadDataIndex1_ID = ReadDataIndex2_Mem_WB) AND MemToRegEnable = '1'
-		ELSE
-		(OTHERS => '0');
-
-	ReadData2_Forward <= ALUOutput_IE_Mem WHEN (RegWriteEnable = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_IE_Mem)
-		ELSE
-		ALUOutput_Mem_WB WHEN (MemWriteEnable = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB) AND MemToRegEnable = '0'
-		ELSE
-		MemOutput_Mem_WB WHEN (MemWriteEnable = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB) AND MemToRegEnable = '1'
+		MemOutput_Mem_WB WHEN (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex1_ID = ReadDataIndex2_Mem_WB) AND MemToRegEnable_MEM_WB = '1'
 		ELSE
 		(OTHERS => '0');
 
-	StallEnable <= '1' WHEN MemRead = '1' AND (ReadDataIndex1_ID_IE = ReadDataIndex2_ID_IE OR ReadDataIndex2_ID = ReadDataIndex2_ID_IE)
+	ReadData2_Forward <= ALUOutput_IE_Mem WHEN (RegWriteEnable_IE_MEM = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_IE_Mem)
+		ELSE
+		ALUOutput_Mem_WB WHEN (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB) AND MemToRegEnable_MEM_WB = '0'
+		ELSE
+		MemOutput_Mem_WB WHEN (RegWriteEnable_MEM_WB = '1' AND ReadDataIndex2_ID_IE = ReadDataIndex2_Mem_WB) AND MemToRegEnable_MEM_WB = '1'
+		ELSE
+		(OTHERS => '0');
+
+	StallEnable <= '1' WHEN MemRead_ID_IE = '1' AND (ReadDataIndex1_ID_IE = ReadDataIndex2_ID_IE OR ReadDataIndex2_ID = ReadDataIndex2_ID_IE)
 		ELSE
 		'0';
 
--- processor.vhd
+++ processor.vhd
@@ -115,12 +115,12 @@ ARCHITECTURE ModelProcessor OF Processor IS
 	END COMPONENT;
 	COMPONENT DataForward IS
 		PORT (
-			MemRead,RegWriteEnable, MemWriteEnable, MemToRegEnable : IN STD_LOGIC;
-			ReadDataIndex2_ID_IE, ReadDataIndex2_IE_Mem, ReadDataIndex2_Mem_WB, ReadDataIndex1_ID_IE : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-			ReadDataIndex1_ID, ReadDataIndex2_ID : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-			ALUOutput_IE_Mem, ALUOutput_Mem_WB, MemOutput_Mem_WB : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ReadData1_Forward_Enable, ReadData2_Forward_Enable, StallEnable : OUT STD_LOGIC;
-			ReadData1_Forward, ReadData2_Forward : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+		MemRead_ID_IE,RegWriteEnable_IE_MEM, RegWriteEnable_MEM_WB, MemToRegEnable_MEM_WB : IN STD_LOGIC;
+		ReadDataIndex2_ID_IE, ReadDataIndex2_IE_Mem, ReadDataIndex2_Mem_WB, ReadDataIndex1_ID_IE : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+		ReadDataIndex1_ID, ReadDataIndex2_ID : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+		ALUOutput_IE_Mem, ALUOutput_Mem_WB, MemOutput_Mem_WB : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		ReadData1_Forward_Enable, ReadData2_Forward_Enable, StallEnable : OUT STD_LOGIC;
+		ReadData1_Forward, ReadData2_Forward : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
 	END COMPONENT;
 	SIGNAL sp, sp_next, sp_out_MEM : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
 	SIGNAL pc, pc_out_IF, pc_next_IF, pc_out_MEM : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
@@ -233,7 +233,7 @@ BEGIN
 
 	-------DataForwarding--------------
 	DF : DataForward PORT MAP(
-		ControlSignals_ID_IE(19),ControlSignals_IE_MEM(6), ControlSignals_MEM_WB(5), ControlSignals_MEM_WB(3),
+		ControlSignals_ID_IE(19),ControlSignals_IE_MEM(6), ControlSignals_MEM_WB(6), ControlSignals_MEM_WB(3),
 		regReadDataIndex2_ID_IE, regReadDataIndex2_IE_MEM, WriteBackIndex, regReadDataIndex1_ID_IE,
 		instruction_IF_ID(7 DOWNTO 5), instruction_IF_ID(2 DOWNTO 0), ALUOutput_IE_MEM, ALUOutput_MEM_WB, MemOutput_MEM_WB,
 		ReadData1_Forward_Enable, ReadData2_Forward_Enable, Stall, ReadData1_Forward_Value, ReadData2_Forward_Value);
